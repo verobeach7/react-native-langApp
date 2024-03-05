013823bb650ab62c50227bc3fd3954357b3026e6
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-03-05 22:22:21 +0900

    React Native Master: Animation(Interpolations)

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Animated, TouchableOpacity } from "react-native";
+import { Animated, Pressable } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -17,34 +17,42 @@ const Box = styled.View`
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  // 애니메이션이 끝나면 toggleUp에 의해 State가 변하므로 리랜더링이 일어남.
-  // 이로 인해 모든 값이 initial value로 재설정되어 원래 자리로 돌아감.
   const [up, setUp] = useState(false);
-  // 리랜더링되면 원래는 초기값으로 리랜더링되지만 useRef를 사용하면 이를 막을 수 있음
-  const Y = useRef(new Animated.Value(0)).current;
-  // up의 value를 바꿔줌
+  const Y_POSITION = useRef(new Animated.Value(300)).current;
   const toggleUp = () => setUp((prev) => !prev);
 
   const moveUp = () => {
-    Animated.timing(Y, {
-      toValue: up ? 200 : -200,
-      // Bridge를 통하지 않고 모든 것을 Native로 보냄
+    Animated.timing(Y_POSITION, {
+      toValue: up ? 300 : -300,
       useNativeDriver: true,
+      duration: 2000,
     }).start(toggleUp);
   };
-  // Animated State는 애니메이션 값을 보여줌
-  Y.addListener(() => console.log("Animated State: ", Y));
-  // Component State는 객체가 랜더링 되었던 위치의 value를 보여줌
-  console.log("Component State: ", Y);
+
+  const opacity = Y_POSITION.interpolate({
+    inputRange: [-300, -100, 100, 300],
+    outputRange: [1, 0, 0, 1],
+  });
+  const borderRadius = Y_POSITION.interpolate({
+    inputRange: [-300, 300],
+    outputRange: [100, 0],
+  });
+  Y_POSITION.addListener(() => {
+    console.log("Y Value: ", Y_POSITION);
+    console.log("opacity Value: ", opacity);
+    console.log("borderRadius Value: ", borderRadius);
+  });
   return (
     <Container>
-      <TouchableOpacity onPress={moveUp}>
+      <Pressable onPress={moveUp}>
         <AnimatedBox
           style={{
-            transform: [{ translateY: Y }],
+            borderRadius,
+            opacity,
+            transform: [{ translateY: Y_POSITION }],
           }}
         />
-      </TouchableOpacity>
+      </Pressable>
     </Container>
   );
 }
