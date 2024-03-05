1f854ed7acc4714855d91e737816c34279fc7519
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-03-05 21:56:05 +0900

    React Native Master: Animation(Animation & State: Change initial value even if re-rendering - useRef)

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Animated, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
@@ -17,21 +17,25 @@ const Box = styled.View`
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  const Y = new Animated.Value(0);
+  // 애니메이션이 끝나면 toggleUp에 의해 State가 변하므로 리랜더링이 일어남.
+  // 이로 인해 모든 값이 initial value로 재설정되어 원래 자리로 돌아감.
+  const [up, setUp] = useState(false);
+  // 리랜더링되면 원래는 초기값으로 리랜더링되지만 useRef를 사용하면 이를 막을 수 있음
+  const Y = useRef(new Animated.Value(0)).current;
+  // up의 value를 바꿔줌
+  const toggleUp = () => setUp((prev) => !prev);
 
   const moveUp = () => {
-    Animated.spring(Y, {
-      toValue: 200,
-      // bounciness와 friction을 함께 사용하지 못함(에러 발생)
-      // bounciness: 50,
-      tension: 50,
-      friction: 1,
+    Animated.timing(Y, {
+      toValue: up ? 200 : -200,
       // Bridge를 통하지 않고 모든 것을 Native로 보냄
       useNativeDriver: true,
-    }).start();
+    }).start(toggleUp);
   };
-  // console.log(Y); // Y값이 변하지 않음(React Component를 리랜더링하는 것이 아니기 때문)
-  Y.addListener(() => console.log(Y));
+  // Animated State는 애니메이션 값을 보여줌
+  Y.addListener(() => console.log("Animated State: ", Y));
+  // Component State는 객체가 랜더링 되었던 위치의 value를 보여줌
+  console.log("Component State: ", Y);
   return (
     <Container>
       <TouchableOpacity onPress={moveUp}>
