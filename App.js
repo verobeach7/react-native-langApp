dcca8d86f1dadbfd305646e9a827ccecd6680d99
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-03-07 23:46:03 +0900

    React Native Master: Animation(Pan Responder #2)

--- App.js
+++ App.js
@@ -29,33 +29,36 @@ export default function App() {
     inputRange: [-300, 300],
     outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"],
   });
-  // state가 변해도 panResponder가 설정된 AnimatedBox는 리랜더링 되지 않도록 useRef 사용
   const panResponder = useRef(
-    // PanResponder 생성
     PanResponder.create({
-      // 터치를 감지하도록 설정
       onStartShouldSetPanResponder: () => true,
-      // 거리가 변하면 이를 AnimatedBox에 적용
       onPanResponderMove: (_, { dx, dy }) => {
-        // 위치값 설정
         POSITION.setValue({
-          // distance of x
           x: dx,
-          // distance of y
           y: dy,
         });
+      },
+      onPanResponderRelease: () => {
+        Animated.spring(POSITION, {
+          toValue: {
+            x: 0,
+            y: 0,
+          },
+          bounciness: 20,
+          useNativeDriver: false,
+        }).start();
       },
     })
   ).current;
   return (
     <Container>
       <AnimatedBox
-        // panHanders가 가지고 있는 function들을 사용할 수 있게 설정
         {...panResponder.panHandlers}
         style={{
           borderRadius,
           backgroundColor: bgColor,
-          transform: [...POSITION.getTranslateTransform()],
+          // css로 변환해줌
+          transform: POSITION.getTranslateTransform(),
         }}
       />
     </Container>
